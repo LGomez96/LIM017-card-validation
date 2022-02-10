1bbfe260dad529d8166d6971d8d1481619493627
LGomez96 <lindagomez96@gmail.com>
2022-02-10 10:05:41 -0500

    primer commit desde Vs code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+const btnSend = document.getElementById("btnSend");
+
+      btnSend.addEventListener("click",()=>{
+         
+        const creditCardNumber = document.getElementById("cardNumber").value;
+                
+        if( validator.isValid(creditCardNumber) == true ){ 
+             alert("Tarjeta válida");
+        } 
+        else {
+             alert("Tarjeta inválida");
+        }
+
+        
+        const creditCardNumber2 = document.getElementById("cardNumber").value;
+
+
+        validator.maskify(creditCardNumber2);
+           
+        //ya es string le dimos una variable nueva para evitar confusiiones con isvalid 
+
+      }   
+
+
+    );
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,51 @@
 const validator = {
   // ...
-};
+ isValid: function IsValidCard (creditCardNumber) {
+  //Para hacer validaciones
+  //Si contiene letras
+  //Si se  ingresa un campo vacío
+    
+  //convirtiendo nuestra cadena en array y revirtiendola
+  const array = Array.from(creditCardNumber).reverse;
+  //creando una variable para sumar.
+  addNumbers = 0;
+  //hacemos el recorrido
+  for (let i =0; i<array.length; i++)
+  //Establecemos condición para el número par. 
+  if ((i+1)%2 === 0){
+    array*=2;
+  if(array[i]>=10){
+    array -= 9;
+    }
+    addNumbers += parseInt(array);
+  }
+  
 
+  
+  
+  
+  }
+
+  
+
+  
+  
+
+ },
+ //..
+
+  maskify: function maskifyCard (creditCardNumber){
+       const hideNum = [];
+       for(let i= 0; i < creditCardNumber.length;i++){
+         if(i< creditCardNumber.lenght -4){
+         hideNum.push("#");
+       } else{
+         hideNum.push (creditCardNumber[i]);
+       }
+       
+      }
+    return hideNum.join("");
+  
+  },
+}; 
 export default validator;
